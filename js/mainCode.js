cabf725b955702e6929507079f5db02cef2d71e3
Adrián Montero de Espinosa <adriianmontero@gmail.com>
2019-01-21 18:00:08 +0100

    First commit
    Skeleton to the new proyect using CSS, HTML, JS, Bootstrap and vars for the CSS (new implementation)

---
+++ js/mainCode.js
@@ -0,0 +1,9 @@
+/**
+window.addEventListener('resize', function() {
+    if (window.innerWidth < 1400) {
+        // Add classes to elements.
+        var mainMenu = document.getElementById('main-menu');
+        mainMenu.classList.add("mystyle");
+    }
+}, false);
+**/
